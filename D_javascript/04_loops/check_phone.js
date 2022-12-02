cb92050b08e4ff3d4a64f1484ca75d5347d71c36
Morning <morning@admins-iMac.local>
2022-12-02 12:59:02 +0800

    zasvar

--- D_javascript/04_loops/check_phone.js
+++ D_javascript/04_loops/check_phone.js
@@ -1,23 +1,15 @@
-// check phone
+check phone
 
-// let number;
+let number;
 
-// while (true) {
-//   number = prompt("enter a phone number:");
-//   let firstChar = number.charAt(0);
-//   number = Number(number);
-//   if (Number.isInteger(number) && (firstChar == "9" || firstChar == "8")) {
-//     break;
-//   }
-// }
-
-// console.log("Access granted");
-
-let i = 0;
-while (i < 10) {
-  i++;
-  if (i % 2 == 0) {
-    continue;
+while (true) {
+  number = prompt("enter a phone number:");
+  let firstChar = number.charAt(0);
+  number = Number(number);
+  if (Number.isInteger(number) && (firstChar == "9" || firstChar == "8")) {
+    break;
   }
-  console.log(i);
 }
+
+console.log("Access granted");
+
